5bb772cac0cb5a954c49671fcbe9a23c6af2ad62
Abraham Elias <abbraham_13905@hotmail.com>
2020-10-31 20:58:40 -0700

    funciones

---
+++ funciones/funciones.py
@@ -0,0 +1,25 @@
+ #def imprimir_mensaje():
+ #   print('Mensaje especial: ')
+ #   print('Aprendiendo funciones: ')
+
+
+#imprimir_mensaje()
+#imprimir_mensaje()
+#imprimir_mensaje()
+
+def conversacion(mensaje):
+    print('Hola')
+    print('como estas')
+    print(mensaje)
+    print('Adios')
+
+
+opcion = int(input('Elige una opcion (1,2,3): '))
+if opcion == 1:
+    conversacion('Eligiste opcion 1')
+elif opcion ==2:
+    conversacion('Eligiste opcion 2')
+elif opcion ==3:
+    conversacion('Eligiste opcion 3')
+else:
+    print('Escribe la opcion correcta')
