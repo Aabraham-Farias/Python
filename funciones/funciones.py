8dfad843625bac1ed6aac948c623ff13be6c5f7e
Abraham Elias <abbraham_13905@hotmail.com>
2020-10-31 21:14:12 -0700

    return valor

--- funciones/funciones.py
+++ funciones/funciones.py
@@ -7,19 +7,27 @@
 #imprimir_mensaje()
 #imprimir_mensaje()
 
-def conversacion(mensaje):
-    print('Hola')
-    print('como estas')
-    print(mensaje)
-    print('Adios')
+#def conversacion(mensaje):
+#    print('Hola')
+#    print('como estas')
+#    print(mensaje)
+#    print('Adios')
 
 
-opcion = int(input('Elige una opcion (1,2,3): '))
-if opcion == 1:
-    conversacion('Eligiste opcion 1')
-elif opcion ==2:
-    conversacion('Eligiste opcion 2')
-elif opcion ==3:
-    conversacion('Eligiste opcion 3')
-else:
-    print('Escribe la opcion correcta')
+#opcion = int(input('Elige una opcion (1,2,3): '))
+#if opcion == 1:
+#    conversacion('Eligiste opcion 1')
+#elif opcion ==2:
+#    conversacion('Eligiste opcion 2')
+#elif opcion ==3:
+#    conversacion('Eligiste opcion 3')
+#else:
+#    print('Escribe la opcion correcta')
+
+def suma(a,b):
+    print('Se suman dos numeros')
+    resultado= a + b 
+    return resultado
+
+sumatoria =suma(2,5)
+print(sumatoria)
